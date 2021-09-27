2996d7c7313a15916cc6f6dee1f4b98daaa0041f
Diegom <diego_r.g@hotmail.com>
2021-09-27 23:46:22 +0200

    first commit

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    MatCardModule,
+    MatTableModule
+  ]
+})
+export class CoursesModule { }
